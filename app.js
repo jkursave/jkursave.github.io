53a5f5cb8f2d1374e21b53199371bfed329d593f
jkursave <jkursave0002@kctcs.edu>
2025-04-13 09:56:06 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-async function getData(selected_major) {
+async function getData(major) {
     var response = await fetch('cit5students.json');   // this is a GET request
 
     if(response.ok) {
         var data = await response.json();
 
         // filter data array for the selected meal
-        major_students = data.filter( (student) => student.major == selected_major );  
+        major_students = data.filter( (student) => student.Major == major );  
 
        var templateText = document.getElementById('citStudents').innerHTML;
        var compiledTemplateText = Handlebars.compile(templateText);   // get and compile template code
