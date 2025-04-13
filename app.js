b6875ce341a3966c9756893a460d7e526667397e
jkursave <jkursave0002@kctcs.edu>
2025-04-13 09:29:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-async function getData(selected_meal) {
+async function getData(selected_major) {
     var response = await fetch('cit students.json');   // this is a GET request
 
     if(response.ok) {
         var data = await response.json();
 
         // filter data array for the selected meal
-        meal_items = data.filter( (item) => item.meal == selected_meal );  
+        major_items = data.filter( (item) => item.major == selected_major );  
 
-       var templateText = document.getElementById('menuTemplate').innerHTML;
+       var templateText = document.getElementById('citStudents').innerHTML;
        var compiledTemplateText = Handlebars.compile(templateText);   // get and compile template code
-       compiledHtml = compiledTemplateText({ rows: meal_items })      // apply data to template
-       document.getElementById('menuTable').innerHTML = compiledHtml; 
+       compiledHtml = compiledTemplateText({ rows: major_items })      // apply data to template
+       document.getElementById('citStudents').innerHTML = compiledHtml; 
     }
     else {
-       document.querySelector('#menuTable').innerHTML = "There was an error, or menu items not found";
+       document.querySelector('#citStudents').innerHTML = "There was an error, or menu items not found";
     }	
  
 }
